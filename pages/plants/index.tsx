afa8929ee927f8823b594dccf67fb1ef59377e9c
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-19 10:37:06 +1100

    feature(plants-page): plants page skeleton

---
+++ pages/plants/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center min-h-screen">
+      <h1 className="text-3xl font-bold">Plants!</h1>
+    </div>
+  );
+};
+
+export default Index;
