9686fcdaee343586bd812535cc2da3f4789ede81
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-19 13:37:14 +1100

    modify(next-config): image domains

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@
 const removeImports = require("next-remove-imports")();
 module.exports = removeImports({
   reactStrictMode: true,
+  images: {
+    domains: [process.env.NEXT_PUBLIC_AWS_BUCKET_URL],
+  },
 });
