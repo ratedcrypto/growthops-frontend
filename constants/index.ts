c3ea9341e399254428d49c1ae6f46ba7ce748f51
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-19 13:23:06 +1100

    feature(plant-add-page): add plant form input validator

--- constants/index.ts
+++ constants/index.ts
@@ -6,3 +6,10 @@ export const ALERT_TEXT =
 export const PLANT_DETAILS_IMAGE_WIDTH = 400;
 export const PLANT_DETAILS_IMAGE_HEIGHT = 400;
 export const PLANT_WATERING_INSTRUCTION = "> Water every other day!";
+export const FILE_SIZE = 2000 * 1500;
+export const SUPPORTED_FORMATS = [
+  "image/jpg",
+  "image/jpeg",
+  "image/gif",
+  "image/png",
+];
