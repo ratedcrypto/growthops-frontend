f7bfd7c05b195bebdcf8e423de2ddebffdb2445f
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-19 11:15:12 +1100

    feature(plants-page): fetch plants in getServerSideProps

--- pages/plants/index.tsx
+++ pages/plants/index.tsx
@@ -1,6 +1,14 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { IPlant } from "../../types";
+import PlantDataService from "../../services/plant.service";
 
-const Index: NextPage = () => {
+type Props = {
+  plantsData: IPlant[];
+  currentPage: number;
+  totalPages: number;
+};
+
+const Index: NextPage<Props> = ({ plantsData, currentPage, totalPages }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center min-h-screen">
       <h1 className="text-3xl font-bold">Plants!</h1>
@@ -9,3 +17,18 @@ const Index: NextPage = () => {
 };
 
 export default Index;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  let plantsData = [];
+  let totalPages = 0;
+  let currentPage = context.query?.page || "1";
+  try {
+    const result = await PlantDataService.getAll({ page: currentPage });
+    plantsData = result.data.data.data;
+    totalPages = result.data.data.last_page;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return { props: { plantsData, currentPage, totalPages } };
+};
