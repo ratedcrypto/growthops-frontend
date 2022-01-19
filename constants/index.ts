87dafbb178875c29c182f3cc0258976555cc5240
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-19 11:26:50 +1100

    feature(plants-page): plants grid and grid cell

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const PLANT_GRID_CELL_IMAGE_WIDTH = 400;
+export const PLANT_GRID_CELL_IMAGE_HEIGHT = 400;
+export const ALERT_TITLE = "Aw, Snap!";
+export const ALERT_TEXT =
+  "Something went wrong while displaying this page. Please contact admin.";
--- pages/plants/index.tsx
+++ pages/plants/index.tsx
@@ -1,6 +1,10 @@
+import React, { useState, useEffect, useRef } from "react";
 import type { GetServerSideProps, NextPage } from "next";
+import { Alert, AlertTitle, CircularProgress } from "@mui/material";
 import { IPlant } from "../../types";
 import PlantDataService from "../../services/plant.service";
+import { ALERT_TEXT, ALERT_TITLE } from "../../constants";
+import PlantGridCell from "../../components/PlantGridCell";
 
 type Props = {
   plantsData: IPlant[];
@@ -9,10 +13,51 @@ type Props = {
 };
 
 const Index: NextPage<Props> = ({ plantsData, currentPage, totalPages }) => {
+  const [plants, setPlants] = useState<IPlant[]>([]);
+  const plantsDataRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (plantsData) {
+      setPlants(plantsData);
+    }
+
+    setLoading(false);
+  }, [plantsData]);
+
   return (
-    <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center min-h-screen">
-      <h1 className="text-3xl font-bold">Plants!</h1>
-    </div>
+    <>
+      <div className="plantsDataRef p-20">
+        {loading ? (
+          <div className="flex items-center justify-center">
+            <CircularProgress color="secondary" />
+          </div>
+        ) : (
+          <div className="mt-5">
+            <h2 className="text-2xl font-bold leading-5 text-gray-900 sm:text-3xl sm:truncate">
+              All Plants
+            </h2>
+            {plants.length > 0 ? (
+              <div>
+                <div
+                  ref={plantsDataRef}
+                  className="grid grid-cols-1 gap-5 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 mt-5"
+                >
+                  {plants.map((plant) => (
+                    <PlantGridCell plant={plant} key={plant.id} />
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <Alert severity="error" variant="outlined">
+                <AlertTitle>{ALERT_TITLE}</AlertTitle>
+                {ALERT_TEXT}
+              </Alert>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
