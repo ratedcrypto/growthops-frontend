f550aca50d5f545324253ec3a6987f4e3a5d5454
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-19 13:04:40 +1100

    feature(plant-add-page): plant data service

--- services/plant.service.ts
+++ services/plant.service.ts
@@ -8,6 +8,15 @@ class PlantDataService {
   get(params: { id: string }) {
     return axios.get(`/v1/plants/${params.id}`);
   }
+
+  add(data: {
+    name: string;
+    species: string;
+    watering_instructions: string;
+    photo: string;
+  }) {
+    return axios.post("/v1/plants", data);
+  }
 }
 
 export default new PlantDataService();
