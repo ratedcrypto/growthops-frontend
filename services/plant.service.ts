6871f10d514481ac10e4cd0244f50f94fb78440b
Rujul Trivedi <rujultrivedi@Rujuls-MacBook-Pro-3.local>
2022-01-19 12:38:32 +1100

    feature(plant-details-page): plant data service

--- services/plant.service.ts
+++ services/plant.service.ts
@@ -4,6 +4,10 @@ class PlantDataService {
   getAll(params: { page: string | string[] }) {
     return axios.get("/v1/plants", { params });
   }
+
+  get(params: { id: string }) {
+    return axios.get(`/v1/plants/${params.id}`);
+  }
 }
 
 export default new PlantDataService();
